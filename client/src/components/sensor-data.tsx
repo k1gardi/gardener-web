766eab452c2721481458e1bb6bba639aa4000f05
k1gardi <55878920+k1gardi@users.noreply.github.com>
2021-10-19 18:52:28 -0500

    initialize firebase

--- client/src/components/sensor-data.tsx
+++ client/src/components/sensor-data.tsx
@@ -1,12 +1,41 @@
+import {Grid, Card, CardContent, Typography} from "@mui/material";
+
 export const SensorData = () => {
   return (
     <div>
       <div>
         <h2>Sensor Data</h2>
       </div>
-      <div>
-        <p>datas</p>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>Sensor 1</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>Sensor 2</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>Sensor 3</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>Sensor 4</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 };
