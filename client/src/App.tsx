2b53b34dabc4472bb49d4f6d8e9051cd7bebbb59
k1gardi <55878920+k1gardi@users.noreply.github.com>
2021-10-20 23:18:50 -0500

    add start button and snackbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
+import {createTheme} from "@mui/material/styles";
+import {ThemeProvider} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 import {WeatherForecast} from "./components/weather-forecast";
 import {SensorData} from "./components/sensor-data";
 import {Controls} from "./components/controls";
 import "./App.css";
-import {createTheme} from "@mui/material/styles";
-import {ThemeProvider} from "@mui/material";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "dark",
   },
 });
 
 export const App = () => {
   return (
     <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <div>
         {/* <header>
         </header> */}
--- client/src/components/controls.tsx
+++ client/src/components/controls.tsx
@@ -1,10 +1,21 @@
 import {useState} from "react";
 import type {ChangeEvent} from "react";
-import {Paper, Box, Typography, TextField} from "@mui/material";
+import {
+  Paper,
+  Box,
+  Alert,
+  TextField,
+  FormGroup,
+  Button,
+  Snackbar,
+  IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const Controls = () => {
   const [timer, setTimer] = useState<string | null>(null);
   const [inputError, setInputError] = useState<boolean>(false);
+  const [snackOn, setSnack] = useState<boolean>(false);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const pattern = new RegExp(/^[0-9]*/);
@@ -17,23 +28,64 @@ export const Controls = () => {
       setInputError(true);
     }
   };
+
+  const handleStart = () => {
+    setSnack(true);
+  };
+
+  const handleCloseSnack = (
+    event: React.SyntheticEvent | React.MouseEvent,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSnack(false);
+  };
+
+  const snackAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={handleCloseSnack}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
   return (
     <div>
       <h2>Controls</h2>
       <Paper elevation={24}>
         <Box p={2}>
-          <Typography>Hello</Typography>
-          <TextField
-            helperText={inputError ? "Time must be <= 30 minutes" : ""}
-            error={inputError}
-            label="Duration"
-            type="number"
-            onChange={handleChange}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            inputProps={{inputMode: "numeric", min: 1, pattern: "[0-9]"}}
-          />
+          <FormGroup row>
+            <TextField
+              helperText={inputError ? "Time must be <= 30 minutes" : ""}
+              error={inputError}
+              label="Duration"
+              type="number"
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{inputMode: "numeric", min: 1, pattern: "[0-9]"}}
+            />
+            <Button variant="contained" onClick={handleStart}>
+              Start
+            </Button>
+            <Snackbar
+              open={snackOn}
+              autoHideDuration={5000}
+              onClose={handleCloseSnack}
+              action={snackAction}
+            >
+              <Alert severity="success" onClose={handleCloseSnack}>
+                This is a success message!
+              </Alert>
+            </Snackbar>
+          </FormGroup>
         </Box>
       </Paper>
     </div>
