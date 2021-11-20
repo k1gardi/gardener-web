75638fb341731871e340701ca5e0fb8eb17af554
k1gardi <55878920+k1gardi@users.noreply.github.com>
2021-11-19 19:18:31 -0600

    add auto water toggle switch

---
+++ src/components/auto-water.tsx
@@ -0,0 +1,82 @@
+import type {FunctionComponent} from "react";
+import {styled} from "@mui/material/styles";
+import {
+  Box,
+  Typography,
+  Switch,
+  Stack,
+} from "@mui/material";
+
+const validateInput = (minutes: string) => {
+  const pattern = new RegExp(/^[0-9]*/);
+  return minutes.match(pattern) && Number(minutes) <= 30;
+};
+
+const padZeros = (number: number) => {
+  return String(number).padStart(2, "0");
+};
+
+export const AutoWater: FunctionComponent<{
+  autoMode: boolean;
+  toggleAutoMode: () => void;
+}> = ({autoMode, toggleAutoMode}) => {
+  return (
+    <Box p={2}>
+      <Typography component="h3">Toggle your smart garden's automatic scheduler</Typography>
+
+      <Stack direction="row" spacing={1} alignItems="center" sx={{paddingTop: "16px",}}>
+        <Typography>Off</Typography>
+        <AntSwitch
+          checked={autoMode}
+          onChange={toggleAutoMode}
+          inputProps={{"aria-label": "ant design"}}
+        />
+        <Typography>On</Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+const AntSwitch = styled(Switch)(({theme}) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,15 +1,27 @@
+import {useState, useCallback} from "react";
 import {Paper, Typography} from "@mui/material";
-import { Box } from "@mui/system";
+import {Box} from "@mui/system";
 import {ManualTimer} from "./manual-control";
+import {AutoWater} from "./auto-water";
+import {useTimer} from "../common/use-timer";
 
 export const Controls = () => {
+  const [autoMode, setAutoMode] = useState<boolean>(false);
+  const {stopTimer} = useTimer();
+
+  const toggleAutoMode = useCallback(() => {
+    setAutoMode(!autoMode);
+    stopTimer()
+  }, [autoMode]);
+
   return (
     <Box p={2}>
       <Typography variant="h3" component="h2">
         Controls
       </Typography>
       <Paper elevation={6}>
-        <ManualTimer />
+        <AutoWater autoMode={autoMode} toggleAutoMode={toggleAutoMode}/>
+        {!autoMode && <ManualTimer />}
       </Paper>
     </Box>
   );
